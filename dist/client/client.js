63429cd2de91f1990acc2a72f5b101bc15afbd36
OmarMWarraich <earthling986@gmail.com>
2023-04-07 05:33:43 +0500

    fix: Dependency

--- dist/client/client.js
+++ dist/client/client.js
@@ -10,4 +10,24 @@ class Client {
             location.reload();
         });
     }
+    showGame(id) {
+        switch (id) {
+            case 0:
+                $('#gamePanel1').fadeOut(100);
+                $('#gamePanel2').fadeOut(100);
+                $('#gamePanel0').delay(100).fadeIn(100);
+                break;
+            case 1:
+                $('#gamePanel0').fadeOut(100);
+                $('#gamePanel2').fadeOut(100);
+                $('#gamePanel1').delay(100).fadeIn(100);
+                break;
+            case 2:
+                $('#gamePanel0').fadeOut(100);
+                $('#gamePanel1').fadeOut(100);
+                $('#gamePanel2').delay(100).fadeIn(100);
+                break;
+        }
+    }
 }
+const client = new Client();
--- src/client/client.ts
+++ src/client/client.ts
@@ -13,4 +13,25 @@ class Client {
             location.reload();
         });
     }
+    public showGame(id: number) {
+        switch (id) {
+            case 0:
+                $('#gamePanel1').fadeOut(100)
+                $('#gamePanel2').fadeOut(100)
+                $('#gamePanel0').delay(100).fadeIn(100)
+                break
+            case 1:
+                $('#gamePanel0').fadeOut(100)
+                $('#gamePanel2').fadeOut(100)
+                $('#gamePanel1').delay(100).fadeIn(100)
+                break
+            case 2:
+                $('#gamePanel0').fadeOut(100)
+                $('#gamePanel1').fadeOut(100)
+                $('#gamePanel2').delay(100).fadeIn(100)
+                break
+        }
+    }
 }
+
+const client = new Client();
